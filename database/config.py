a7d3a07fadd2d7fca4f5b127bd64cdd8b469eca2
Timur-ABS <83013236+Timur-ABS@users.noreply.github.com>
2023-07-01 00:33:00 +0300

    Update config.py

--- database/config.py
+++ database/config.py
@@ -1,7 +1,7 @@
 db_config = {
     'user': 'timur',
-    'password': 'zgABukMTayBtdycLO7V7RQPSpLHjLjBT5AFKXwXulYSVho9IJ4a8NAdmAbbJL7jE!',
+    'password': '',
     'host': '51.250.64.64',
-    'db': 'database',
-    'port': 3306
+    'db': '',
+    'port': 
 }
